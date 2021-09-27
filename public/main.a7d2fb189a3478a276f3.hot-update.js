7560d74b5663fb4982cc7341d87ed1246a9372b5
Hannah Leggett <hannahlleggett@gmail.com>
2021-09-27 22:57:04 +0100

    updates styles

---
+++ public/main.a7d2fb189a3478a276f3.hot-update.js
@@ -0,0 +1,2 @@
+"use strict";self.webpackHotUpdatediamond("main",{},(function(e){e.h=()=>"583b27e04845d85de49b"}));
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5hN2QyZmIxODlhMzQ3OGEyNzZmMy5ob3QtdXBkYXRlLmpzIiwibWFwcGluZ3MiOiJpRUFBQUEsRUFBb0JDLEVBQUksSUFBTSIsInNvdXJjZXMiOlsid2VicGFjazovL2RpYW1vbmQvd2VicGFjay9ydW50aW1lL2dldEZ1bGxIYXNoIl0sInNvdXJjZXNDb250ZW50IjpbIl9fd2VicGFja19yZXF1aXJlX18uaCA9ICgpID0+IChcIjU4M2IyN2UwNDg0NWQ4NWRlNDliXCIpIl0sIm5hbWVzIjpbIl9fd2VicGFja19yZXF1aXJlX18iLCJoIl0sInNvdXJjZVJvb3QiOiIifQ==
